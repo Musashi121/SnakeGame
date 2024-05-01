c0a32b434d43ff61ede2215801ae71d31e8bbabb
Michalis <michalisjk@gmail.com>
2024-05-01 12:10:26 +0100

    Day 2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Generic Squamate Reptile Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,8 +23,17 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-
+    time.sleep(0.05)
     snake.move()
+    scoreboard.update_scoreboard()
+    # Detect collision with food, using turtle.distance
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-MOVE_DISTANCE = 20
+MOVE_DISTANCE = 15
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -17,7 +17,7 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("circle")
+            new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
